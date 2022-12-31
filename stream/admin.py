8965f02e5947bc8aad50e012c449f128e5d43c37
Julian Hinsch <jhinsch799@gmail.com>
2022-12-31 18:43:19 -0500

    add stream app

---
+++ stream/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import WatchItem, ListenItem
+
+admin.site.register(WatchItem)
+admin.site.register(ListenItem)
---
+++ stream/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('schedule', views.schedule, name='schedule'),
+    path('listen', views.listen, name='listen'),
+    path('watch', views.watch, name='watch'),
+]
