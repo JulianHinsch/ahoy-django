7fffdda3f9c8c60cd2b1f04de5d976d7f1f8d6d0
Julian Hinsch <jhinsch799@gmail.com>
2023-01-06 11:36:02 -0500

    watch and listen pages functional with zero state

--- stream/urls.py
+++ stream/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('schedule', views.schedule, name='schedule'),
-    path('listen', views.listen, name='listen'),
-    path('watch', views.watch, name='watch'),
+    path('schedule/', views.schedule, name='schedule'),
+    path('listen/', views.listen, name='listen'),
+    path('watch/', views.watch, name='watch'),
 ]
--- stream/views.py
+++ stream/views.py
@@ -1,23 +1,28 @@
-from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from django.shortcuts import get_object_or_404
-from django.contrib.auth.decorators import login_required, user_passes_test
-from django.contrib.auth import authenticate, login, logout
+from datetime import datetime
 
 from .models import WatchItem, ListenItem
 
 def schedule(request):
     context = {
-        'watch_items': WatchItem.objects.all(),
-        'listen_items': ListenItem.objects.all(),
+        'watch_items': WatchItem.objects.filter(start_time__date = datetime.today()),
+        'listen_items': ListenItem.objects.filter(start_time__date = datetime.today()),
     }
     return render(request, 'schedule.html', context)
 
-def watch(request, stream_id):
-    context = {'watch_item': get_object_or_404(WatchItem, id = stream_id)}
+def watch(request):
+    try:
+        current_item = WatchItem.objects.filter(start_time__gte = datetime.now()).earliest('start_time')
+    except WatchItem.DoesNotExist:
+        current_item = None
+    context = {'watch_item': current_item }
     return render(request, 'watch.html', context)
 
 
-def listen(request, stream_id):
-    context = {'watch_item': get_object_or_404(WatchItem, id = stream_id)}
+def listen(request):
+    try:
+        current_item = ListenItem.objects.filter(start_time__gte = datetime.now()).earliest('start_time')
+    except ListenItem.DoesNotExist:
+        current_item = None
+    context = {'listen_item': current_item }
     return render(request, 'listen.html', context)
