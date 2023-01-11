df2a47d8bb31fafea491f82d088e5ac217aa4024
Julian Hinsch <jhinsch799@gmail.com>
2023-01-11 16:15:47 -0500

    updates for heroku

--- ahoy/settings.py
+++ ahoy/settings.py
@@ -146,7 +146,7 @@ STATIC_ROOT = BASE_DIR / 'static'
 
 STATIC_URL = 'static/'
 
-STATICFILES_STORAGE =  'django.contrib.staticfiles.storage.StaticFilesStorage' 
+STATICFILES_STORAGE =  'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
